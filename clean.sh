d5c4090d4301db3988d95de0a88f7a24515b1194
cuiziqiang <cuiziqiang@tju.edu.cn>
2019-08-01 14:37:25 +0800

    updated

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-FILES="*.aux .bb *.bak *.bl *.lo *.lo *.nl *.ou *.to *.bc *.run.xml *.bbl *.blg *.fdb_latexmk *.fls *.log *.toc *.synctex.gz *.acn *.glo *.ist *.lof *.lot *.out *.acr *.alg *.glg *.gls *.glsdefs"
+FILES="*.aux .bb *.bak *.bl *.lo *.lo *.nl *.ou *.to *.bc *.run.xml *.bbl *.blg *.fdb_latexmk *.fls *.log *.toc *.synctex.gz *.acn *.glo *.ist *.lof *.lot *.out *.acr *.alg *.glg *.gls *.glsdefs *.sav"
 rm -f $FILES
 cd abstract
 rm -f $FILES
